b7539da1238aec4429a71cdf1dd267e17581336a
RitalI <344021324@tdsb.ca>
2026-06-12 17:59:30 -0400

    Wrap MainLayout with single RouteGuard instead of protecting each route individually

--- src/App.tsx
+++ src/App.tsx
@@ -29,102 +29,38 @@ import {
 export const App = () => {
   return (
     <Routes>
-      <Route element={<MainLayout />}>
+      <Route element={<SignInView />} path="/sign-in" />
+
+      <Route
+        element={
+          <RouteGuard>
+            <MainLayout />
+          </RouteGuard>
+        }
+      >
         <Route element={<HomeView />} path="/" />
-        <Route element={<SignInView />} path="/sign-in" />
-
         <Route element={<Navigate replace to="/movies/category/now_playing" />} path="movies" />
-        <Route
-          element={
-            <RouteGuard>
-              <MoviesView />
-            </RouteGuard>
-          }
-          path="movies/category/:interval"
-        />
-
+        <Route element={<MoviesView />} path="movies/category/:interval" />
         <Route element={<Navigate replace to="/tv/category/airing_today" />} path="tv" />
-        <Route
-          element={
-            <RouteGuard>
-              <TelevisionView />
-            </RouteGuard>
-          }
-          path="tv/category/:interval"
-        />
-
+        <Route element={<TelevisionView />} path="tv/category/:interval" />
         <Route element={<Navigate replace to="/trending/movie" />} path="trending" />
-        <Route
-          element={
-            <RouteGuard>
-              <TrendingView />
-            </RouteGuard>
-          }
-          path="trending/:mediaType"
-        />
-
-        <Route
-          element={
-            <RouteGuard>
-              <GenreView />
-            </RouteGuard>
-          }
-          path="genre"
-        />
-        <Route
-          element={
-            <RouteGuard>
-              <GenreView />
-            </RouteGuard>
-          }
-          path="genre/:type/:genreSlug"
-        />
-
-        <Route
-          element={
-            <RouteGuard>
-              <SearchView />
-            </RouteGuard>
-          }
-          path="search"
-        />
-
-        <Route
-          element={
-            <RouteGuard>
-              <PersonView />
-            </RouteGuard>
-          }
-          path="person/:id"
-        >
+        <Route element={<TrendingView />} path="trending/:mediaType" />
+        <Route element={<GenreView />} path="genre" />
+        <Route element={<GenreView />} path="genre/:type/:genreSlug" />
+        <Route element={<SearchView />} path="search" />
+        <Route element={<PersonView />} path="person/:id">
           <Route element={<Navigate replace to="career" />} index />
           <Route element={<CareerView />} path="career" />
           <Route element={<ImagesView />} path="images" />
         </Route>
-
-        <Route
-          element={
-            <RouteGuard>
-              <MovieView />
-            </RouteGuard>
-          }
-          path="movie/:id"
-        >
+        <Route element={<MovieView />} path="movie/:id">
           <Route element={<Navigate replace to="summary" />} index />
           <Route element={<SummaryView />} path="summary" />
           <Route element={<CreditsView />} path="credits" />
           <Route element={<TrailersView />} path="trailers" />
           <Route element={<ReviewsView />} path="reviews" />
         </Route>
-
-        <Route
-          element={
-            <RouteGuard>
-              <MovieView />
-            </RouteGuard>
-          }
-          path="tv/:id"
-        >
+        <Route element={<MovieView />} path="tv/:id">
           <Route element={<Navigate replace to="summary" />} index />
           <Route element={<SummaryView />} path="summary" />
           <Route element={<CreditsView />} path="credits" />
@@ -133,43 +69,10 @@ export const App = () => {
           <Route element={<SeasonsView />} path="seasons" />
           <Route element={<EpisodeView />} path="season/:seasonNumber" />
         </Route>
-
-        <Route
-          element={
-            <RouteGuard>
-              <FavoritesView />
-            </RouteGuard>
-          }
-          path="favorites"
-        />
-
-        <Route
-          element={
-            <RouteGuard>
-              <CartView />
-            </RouteGuard>
-          }
-          path="cart"
-        />
-
-        <Route
-          element={
-            <RouteGuard>
-              <SuccessView />
-            </RouteGuard>
-          }
-          path="success"
-        />
-
-        <Route
-          element={
-            <RouteGuard>
-              <SettingsView />
-            </RouteGuard>
-          }
-          path="settings"
-        />
-
+        <Route element={<FavoritesView />} path="favorites" />
+        <Route element={<CartView />} path="cart" />
+        <Route element={<SuccessView />} path="success" />
+        <Route element={<SettingsView />} path="settings" />
         <Route element={<ErrorView />} path="*" />
       </Route>
     </Routes>
